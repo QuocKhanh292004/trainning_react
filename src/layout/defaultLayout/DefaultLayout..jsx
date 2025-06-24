c7390e60dd18b5b9f0ad1575099cd383885083e3
QuocKhanh292004 <t.quockhanh02092004@gmail.com>
2025-06-24 15:39:55 +0700

    layout_main

--- src/layout/defaultLayout/DefaultLayout..jsx
+++ src/layout/defaultLayout/DefaultLayout..jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import Header from "../../components/header";
-import Content from "../../components/content";
-import Footer from "../../components/footer";
+// layout toàn trang
+import React from 'react';
+import Header from '../../components/header';
+import Content from '../../components/content';
+import Footer from '../../components/footer';
 
 function DefaultLayout() {
-  return (
-    <div>
-      <div>
-        <Header></Header>
-      </div>
-      <div>
-        <Content></Content>
-      </div>
-      <div>
-        <Footer></Footer>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <Header></Header>
+            </div>
+            <div>
+                <Content></Content>
+            </div>
+            <div>
+                <Footer></Footer>
+            </div>
+        </div>
+    );
 }
 
 export default DefaultLayout;
