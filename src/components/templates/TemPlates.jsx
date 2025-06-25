fdf1bdc854a4a50e1bd8657497e86db698272d71
QuocKhanh292004 <t.quockhanh02092004@gmail.com>
2025-06-25 21:30:18 +0700

    Add Templates content

--- src/components/templates/TemPlates.jsx
+++ src/components/templates/TemPlates.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TemplateCard from "./TemplateCard";
+import { motion } from "framer-motion";
 
 // Bạn có thể thay thế đường dẫn ảnh thật ở đây
 const templates = [
@@ -97,25 +98,32 @@ const templates = [
 
 function Templates() {
   return (
-    <div className="py-16 px-6 md:px-20 bg-white">
-      <h2 className="text-5xl text-center font-pacifico text-gray-700">
-        15 Templates
-      </h2>
-      <h3 className="text-2xl text-center font-oswald font-bold mt-2">
-        HOMEPAGES
-      </h3>
+      <div className="py-16 px-6 md:px-20 bg-white">
+        <h2 className="text-5xl text-center font-pacifico text-gray-700">
+          {templates.length} Templates
+        </h2>
+        <h3 className="text-2xl text-center font-oswald font-bold mt-2">
+          HOMEPAGES
+        </h3>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-12">
-        {templates.map((tpl, index) => (
-          <TemplateCard
-            key={index}
-            image={tpl.image}
-            title={tpl.title}
-            link={tpl.link}
-          />
-        ))}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-12">
+          {templates.map((tpl, index) => (
+              <motion.div
+                  key={index}
+                  initial={{ opacity: 0, y: 40 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  viewport={{ once: true, amount: 0.0 }}
+                  transition={{ duration: 0.6, ease: "easeOut", delay: index * 0.1 }}
+              >
+                <TemplateCard
+                    image={tpl.image}
+                    title={tpl.title}
+                    link={tpl.link}
+                />
+              </motion.div>
+          ))}
+        </div>
       </div>
-    </div>
   );
 }
 
